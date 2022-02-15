5c905be5a332204423d6352c03bad54c88c33f83
ericosg <eric@loadx.org>
2022-02-15 08:26:58 +0200

    vote 2 parsed

--- app.js
+++ app.js
@@ -5,24 +5,30 @@ const fs = require('fs');
 let holdersJson = fs.readFileSync('gib-holders.json');
 let holders = JSON.parse(holdersJson);
 
-let dbJson = fs.readFileSync('oikattoi-default-rtdb-export.json');
+let dbJson = fs.readFileSync('oikattoi-default-rtdb-export_v2.json');
 let db = JSON.parse(dbJson);
 
 let votes = [0, 0, 0, 0, 0, 0, 0, 0];
 
+const voteToCount = 2;
+
 for (const wallet in db.wallets) {
     if (Object.hasOwnProperty.call(db.wallets, wallet)) {
         const element = db.wallets[wallet];
-        const nfts = _.pick(holders, [wallet]);
-        if (nfts && nfts[wallet]) {
-            const amount = nfts[wallet].amount;
-            if (votes[element.choiceId]) {
-                votes[element.choiceId] += amount;
+        if (element.voteId == voteToCount) {
+            const nfts = _.pick(holders, [wallet]);
+            if (nfts && nfts[wallet]) {
+                const amount = nfts[wallet].amount;
+                if (votes[element.choiceId]) {
+                    votes[element.choiceId] += amount;
+                } else {
+                    votes[element.choiceId] = amount;
+                }
             } else {
-                votes[element.choiceId] = amount;
+                console.log("Wallet not found", wallet);
             }
         } else {
-            console.log("Wallet not found", wallet);
+            console.log("Voted before, but not this time", wallet);
         }
     }
 }
